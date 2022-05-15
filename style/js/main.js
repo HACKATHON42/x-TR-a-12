470eb1ce6eb667bc92c6166ce63abcfab33bde7a
Mücahit Buğday <mucahit_bugday@outlook.com>
2022-05-15 03:19:57 +0300

    ürün satış sayfası 100%

--- style/js/main.js
+++ style/js/main.js
@@ -1,16 +1,3 @@
-var deger;
-var saniye=600;
-function saniyeDurdur()
-  {
-	window.clearInterval(deger);
-  }
-function saniyeBaslat()
-{
-	saniye --;
-	if(saniye >=0){
-		document.getElementById('saniye').innerHTML=saniye;
-	}else{
-		window.clearInterval(deger);
-	}
-}
-var deger=window.setInterval('saniyeBaslat()',1000);
+$('#myModal').on('shown.bs.modal', function () {
+	$('#myInput').trigger('focus')
+  })
